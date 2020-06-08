a6f475d36fb5cb99f34d1ddb229328d608b38a26
SidBitout <34778272+SidBitout@users.noreply.github.com>
2020-06-08 19:52:00 +0100

    Updates

---
+++ precache-manifest.02eac6f74c3e9cc274a97caa44068050.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296f843b87a21cb03f9a3926138004dc",
+    "url": "/myrobofriends/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/myrobofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7a263cb3abe05ff7eb7f",
+    "url": "/myrobofriends/static/css/main.e0f6a8f7.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/myrobofriends/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myrobofriends/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a263cb3abe05ff7eb7f",
+    "url": "/myrobofriends/static/js/main.6f026c65.chunk.js"
+  },
+  {
+    "revision": "ba2ae51139c8f03741dd",
+    "url": "/myrobofriends/static/js/runtime-main.d1861646.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
